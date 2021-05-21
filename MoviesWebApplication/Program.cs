27f5a0b1e27588a98a9a6ff2537e5f4d5342fa86
karlich99 <33891069+karlajajic@users.noreply.github.com>
2021-05-22 00:39:26 +0200

    start database

--- MoviesWebApplication/Program.cs
+++ MoviesWebApplication/Program.cs
@@ -13,9 +13,31 @@ namespace MoviesWebApplication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
         }
 
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<MoviesDBContext>();
+                    context.Database.EnsureCreated();
+                    // DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
