b02711c458e7bd83ff665ce143f6ca68767e006c
lenabojanowska <lena.bojanowska99@gmail.com>
2021-05-21 09:39:39 +0200

    Initial commit

---
+++ MoviesWebApplication/Models/MoviesModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesWebApplication.Models
+{
+    public class MoviesModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+        public enum ECategory { Action = 0, Adventure = 1, Horror = 2, Drama = 3, Romance = 4, Comedy = 5 }
+    }
+}
