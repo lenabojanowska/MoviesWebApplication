ebadee66d9b98a9167491c339dbf33b231cce306
karlich99 <33891069+karlajajic@users.noreply.github.com>
2021-05-25 11:56:01 +0200

    changed log in

--- MoviesWebApplication/Startup.cs
+++ MoviesWebApplication/Startup.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using MoviesWebApplication.Data;
 using System;
 using System.Collections.Generic;
@@ -27,16 +31,24 @@ namespace MoviesWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(AzureADDefaults.AuthenticationScheme).AddAzureAD(
-                options=> Configuration.Bind("AzureAd",options));
+            //services.AddAuthentication(AzureADDefaults.AuthenticationScheme).AddAzureAD(
+            //    options => Configuration.Bind("AzureAd", options));
 
-            //services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            //    .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
 
             services.AddDbContext<MoviesContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("MoviesContext")));
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddRazorPages().AddMicrosoftIdentityUI();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
